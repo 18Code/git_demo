8782b099c3521a476a37a54cb3b8dbd994c3e3b8
gongfeifei <15230810870@163.com>
2018-07-20 16:01:32 +0800

    提交注释

--- src/com/chapter8/ThisTest.java
+++ src/com/chapter8/ThisTest.java
@@ -10,9 +10,9 @@ public class ThisTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		SimpleTime time = new SimpleTime(12, 30, 19);
+		SimpleTime time = new SimpleTime(12, 30, 19);	//创建并初始化SimpleTime对象
 		
-		JOptionPane.showMessageDialog(null, time.buildString(), "Demostrating the \"this\" Reference", JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, time.buildString(), "Demostrating the \"this\" Reference", JOptionPane.INFORMATION_MESSAGE);	//在弹出框输出buildString方法返回的字符串
 		
 		System.exit(0);
 	}
@@ -20,13 +20,13 @@ public class ThisTest {
 }
 
 class SimpleTime {
-	private int hour;
+	private int hour;	//声明三个private实例变量
 	private int minute;
 	private int second;
 
-	public SimpleTime(int hour, int minute, int second) {
+	public SimpleTime(int hour, int minute, int second) {	//构造函数，接收三个int型参数以初始化一个SimpleTime对象
 		super();
-		this.hour = hour;
+		this.hour = hour;	//使用this显式地引用屏蔽的SimpleTime类的实例变量
 		this.minute = minute;
 		this.second = second;
 	}
