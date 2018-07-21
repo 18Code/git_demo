5fed70b7ec451f83c465af41275fec42ad773666
gongfeifei <15230810870@163.com>
2018-07-21 16:50:44 +0800

    Employee2 part1

--- src/com/chapter8/Employee2.java
+++ src/com/chapter8/Employee2.java
@@ -1,5 +1,18 @@
 package com.chapter8;
 
-public class Employee2 {
+public class Employee2 {	//Employee2声明
 
+	private String firstName;	//声明姓氏
+	private String lastName;	//声明名字
+	private static int count = 0;	//声明类变量count
+	public Employee2(String firstName, String lastName) {	//构造方法初始化姓氏和名字
+		super();
+		this.firstName = firstName;	
+		this.lastName = lastName;
+		
+		++count;	//静态变量员工的数量增1
+		System.out.println("Employee constructor: " +
+				firstName + " " + lastName);	//打印员工信息
+	}
+	
 }
