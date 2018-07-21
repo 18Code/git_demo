16236a3642054f959be8771b3e10fc90ef67a3da
Shasha6666 <1620309354@qq.com>
2018-07-21 16:19:48 +0800

    Data改名为Date类 EmployeeTest类all

--- src/com/chapter8/Employee.java
+++ src/com/chapter8/Employee.java
@@ -4,9 +4,9 @@ public class Employee {	//Employee类声明
 
 	private String firstName;	//声明姓氏
 	private String lastName;	//声明名字
-	private Data brithData;		//声明出生日期
-	private Data hireData;		//声明入职日期
-	public Employee(String firstName, String lastName, Data brithData, Data hireData) {	//构造方法初始化姓名，出生日期和入职日期
+	private Date brithData;		//声明出生日期
+	private Date hireData;		//声明入职日期
+	public Employee(String firstName, String lastName, Date brithData, Date hireData) {	//构造方法初始化姓名，出生日期和入职日期
 		super();
 		this.firstName = firstName;	
 		this.lastName = lastName;
