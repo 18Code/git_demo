90a8e2579448663377dc7819d8c2a89196dfea70
Shasha6666 <1620309354@qq.com>
2018-07-21 16:45:24 +0800

    part2

--- src/com/chapter8/Employee2.java
+++ src/com/chapter8/Employee2.java
@@ -1,5 +1,25 @@
 package com.chapter8;
 
 public class Employee2 {
+	private String firstName;
+	private String lastName;
+	private static int count = 0;
 
-}
+	protected void finalize(){
+		--count;
+		System.out.println("Employee finalizer: "+firstName+" "+lastName+" ; count= "+count);
+		
+	}//end finalize
+	
+	public String getFirstName(){
+		return firstName; //返回firstName
+	}//end getFirstName
+	
+	public String getLastName(){
+		return lastName;   //返回lastName
+	}//end getLastName
+	
+	public static int getCount(){
+		return count;    //返回count
+	}
+}// end class Employee
--- src/com/chapter8/EmployeeTest2.java
+++ src/com/chapter8/EmployeeTest2.java
@@ -3,7 +3,7 @@ package com.chapter8;
 public class EmployeeTest2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Employee e1 = new Employee("Susan","Baker");
 		
 	}
 
