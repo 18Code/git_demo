98ada5263ef0107e00e92a67291ec7d230bea62d
Shasha6666 <1620309354@qq.com>
2018-07-22 08:32:44 +0800

    part1

---
+++ src/com/chapter8/IncrementTest.java
@@ -0,0 +1,28 @@
+package com.chapter8;
+
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JApplet;
+import javax.swing.JButton;
+
+public class IncrementTest extends JApplet implements ActionListener{
+	private Increment incrementObject;
+	private JButton button;
+	
+	public void init(){
+		incrementObject = new Increment(5);
+		Container container = getContentPane();
+		
+		button = new JButton("Click to increment");
+		button.addActionListener(this);
+		container.add(button);
+	}//end init
+	
+	public void actionPerformed(ActionEvent actionEvent){
+		incrementObject.increment();
+		showStatus(incrementObject.toIncrementString());
+	}//end actionPerformed
+
+}//end class Increment
