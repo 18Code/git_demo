d0c500f262beb99d6740c44e5449761ae269e8f1
Shasha6666 <1620309354@qq.com>
2018-07-24 09:54:12 +0800

    注释

--- src/com/chapter9/PointTest.java
+++ src/com/chapter9/PointTest.java
@@ -7,11 +7,13 @@ public class PointTest {
 		Point point = new Point(72,115);
 		
 		String output ="";
-		output+="\n\nThe new location of point is "+point;
+		output+="\n\nThe new location of point is "+point;  //拼接字符串，此处隐式调用Object的toString方法
 		
+		//调用showMessageDialog方法，输出output字符串
 		JOptionPane.showMessageDialog(null, output);
 		
+		//退出系统
 		System.exit(0);
-	}
+	}//end main
 
-}
+}// end class PointTest
