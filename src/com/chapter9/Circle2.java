de3efbc7bf763c80bb097c4adaa7f3f1f967bbe2
gongfeifei <15230810870@163.com>
2018-07-25 09:02:10 +0800

    Cricle2 part1

---
+++ src/com/chapter9/Circle2.java
@@ -0,0 +1,24 @@
+package com.chapter9;
+
+public class Circle2 extends Point {	//声明Circle2类，继承自Point
+	private double radius;    //声明私有变量---半径
+
+	public Circle2() {	//无参构造函数
+		//Point构造方法的隐式调用
+	}
+	
+	public Circle2(int xValue, int yValue, double radiusValue) {	//带三个参数的构造方法
+		//x = xValue;    //x赋值为xValue。报错，x是Point类的私有成员变量，它的子类不能直接访问它的私有成员
+		//y = yValue;    //y赋值为yValue。报错
+		setRadius(radiusValue);    //调用setRadius设置radius
+	}
+	
+	public void setRadius(double radiusValue) {	//setRadius方法，设置圆半径
+		radius = (radiusValue < 0.0 ? 0.0 : radiusValue);	//如果radiusValue值小于0，radius置为0；不小于0，radius置为radiusValue
+	}
+
+	public double getRadius() {	//getRadius方法
+		return radius;	//返回radius
+	}
+	
+}
