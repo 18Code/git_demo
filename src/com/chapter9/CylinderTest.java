c1b956214f8d7fd2a7e1ea4c9721cf075d8ff534
Shasha6666 <1620309354@qq.com>
2018-07-25 11:30:21 +0800

    emmm

--- src/com/chapter9/CylinderTest.java
+++ src/com/chapter9/CylinderTest.java
@@ -2,6 +2,8 @@ package com.chapter9;
 
 import java.text.DecimalFormat;
 
+import javax.swing.JOptionPane;
+
 public class CylinderTest {
 
 	public static void main(String[] args) {
@@ -21,7 +23,19 @@ public class CylinderTest {
 		output += "\n\n The new location, radius and height of cylinder are\n" +
 				cylinder.toString();	//调用cylinder的toString方法，以获取cylinder对象的字符串表示，并将返回结果拼接到output上
 		
-		DecimalFormat twoDigits = new DecimalFormat("0.00");	//创建DecimalFormat对象，用于将数值格式化为小数点后两位数字
+		DecimalFormat twoDigitals = new DecimalFormat("0.00");	//创建DecimalFormat对象，用于将数值格式化为小数点后两位数字
+		//构造输出字符串，调用getDiameter方法，输出圆柱的直径
+		output+="\n\nDiameter is "+twoDigitals.format(cylinder.getDiameter());
+		//构造输出字符串，调用getCirclefereccn方法，输出圆柱的周长
+		output+="\n\nCircumference is "+twoDigitals.format(cylinder.getCircleference());
+		//构造输出字符串，输出圆柱的表面积
+		output+="\n\nArea is "+twoDigitals.format(cylinder.getArea());
+		//构造输出字符串，输出圆柱的体积
+		output+="\n\nVolume is "+twoDigitals.format(cylinder.getVolume());
+		//将输出字符串显示在对话框中
+		JOptionPane.showMessageDialog(null, output);
+				
+				System.exit(0);
 	}
 
 }
