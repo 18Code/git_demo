1c871955923bb687a32c34779fc59f14ce646c37
gongfeifei <15230810870@163.com>
2018-07-26 09:17:20 +0800

    ConstructorFinalizerTest part2

--- src/com/chapter9/ConstructorFinalizerTest.java
+++ src/com/chapter9/ConstructorFinalizerTest.java
@@ -13,6 +13,14 @@ public class ConstructorFinalizerTest {
 		System.out.println();   //输出空行
 		circle2 = new Circle5(5,7,10.67);   //调用构造方法，为circle2对象实例化
 		
+		point = null;	//将point设置为null，做标记用于垃圾收集
+		circle1 = null;	//将circle1设置为null
+		circle2 = null;	//将circle2设置为null
+		
+		System.out.println();	//输出空行
+		
+		System.gc();	//调用垃圾收集方法
+		
 	}
 
 }
