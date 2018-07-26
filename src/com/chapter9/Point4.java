76756abadf8bf44b9349c91b1025debf07d0c214
gongfeifei <15230810870@163.com>
2018-07-26 09:21:58 +0800

    修改

--- src/com/chapter9/Point4.java
+++ src/com/chapter9/Point4.java
@@ -9,6 +9,8 @@ public class Point4 {	//声明Point类，代表xy轴坐标对
 	}
 	
 	public Point4(int xValue, int yValue) {	//带两个参数的构造方法
+		setX(xValue);
+		setY(yValue);
 		System.out.println("Point4 constructor: " + this);	//打印Point4对象调用了有参构造函数
 	}
 	
