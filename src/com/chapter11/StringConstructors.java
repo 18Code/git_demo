8c91d0ff1818beb5cd7f90750d0105f8b381257f
wizcc <294116824@qq.com>
2018-08-09 11:27:49 +0800

    11.1

---
+++ src/com/chapter11/StringConstructors.java
@@ -0,0 +1,20 @@
+package com.chapter11;
+
+import javax.swing.JOptionPane;
+
+//String类的构造函数
+
+public class StringConstructors {
+	
+	public static void main(String[] args) {
+		
+		char charArray[] = { 'b', 'i', 't', 'h', ' ', 'd', 'a', 'y' };
+		byte byteArray[] = { ( byte ) 'n', ( byte ) 'e', ( byte ) 'w', ( byte ) ' ', ( byte ) 'y', 
+							( byte ) 'e', ( byte ) 'a',  ( byte ) 'r' };
+		
+		String s = new String( "hello" );
+		
+		
+	}
+
+}
