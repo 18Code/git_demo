78bcce0cc783cb375d0913d44eef69744635f56a
wizcc <294116824@qq.com>
2018-08-09 11:48:51 +0800

    11章的部分代码

---
+++ src/com/chapter11/StringMiscellaneous.java
@@ -0,0 +1,15 @@
+package com.chapter11;
+
+public class StringMiscellaneous {
+	public static void main(String[] args) {
+		
+		String output = "s1: " + s1;
+		
+		//测试length方法
+		output += "\nLength of s1: " + s1.length();
+		
+		//遍历s1中的所有字符并逆序表示
+		output += "\nThe string reversed is: "; 
+	}
+
+}
