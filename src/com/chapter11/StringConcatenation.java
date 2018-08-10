3576b97a750c073f88e69b021cd71aa76b3a78de
teamOfSy <407626072@qq.com>
2018-08-10 23:39:22 +0800

    缺part 3 part 4

--- src/com/chapter11/StringConcatenation.java
+++ src/com/chapter11/StringConcatenation.java
@@ -3,10 +3,18 @@ package com.chapter11;
 public class StringConcatenation {
 	public static void main(String[] args) {
 		
+		// 创建两个String对象分别把它们的引用赋给s1和s2变量
+		String s1 = new String( "Happy" );
+		String s2 = new String( "Birthday" );
+		
 		String output = "s1 = " + s1 + "\ns2 = " + s2;
 		
 		output += "\n\nResult of s1.concat( s2 ) = " + s1.concat( s2 );
 		output += "\ns1 after concatenation = " + s1;
-	}
-
-}
+		
+	
+		System.exit( 0 ); // terminate application
+		
+	} // end method main
+	
+} // end class StringConcatenation
