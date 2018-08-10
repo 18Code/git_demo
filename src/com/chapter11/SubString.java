4677ba1a1240f3ad2ce82881a81247d33a91de32
teamOfSy <407626072@qq.com>
2018-08-10 22:05:44 +0800

    some modify

--- src/com/chapter11/SubString.java
+++ src/com/chapter11/SubString.java
@@ -5,16 +5,21 @@ import javax.swing.JOptionPane;
 //11.6
 public class SubString {
 	public static void main(String[] args) {
+		
 		String letters = "abcdefghijklmabcdefghijklm";	//声明字符串变量letters并赋值
 		
 		//测试subString方法
-		String output = "Substring from index 20 to end is " + "\"" + letters.substring( 20 ) + "\"\n";
-		output += "Substring from index 3 up to 6 is " + "\"" + letters.substring( 3, 6 ) + "\"";
+		String output = "Substring from index 20 to end is " 
+			+ "\"" + letters.substring( 20 ) + "\"\n";
+		output += "Substring from index 3 up to 6 is "
+			+ "\"" + letters.substring( 3, 6 ) + "\"";
 		
-		JOptionPane.showMessageDialog(null, output,
-				"String substring methods", JOptionPane.INFORMATION_MESSAGE);	//输出output
+		JOptionPane.showMessageDialog( null, output,
+			"String substring methods", 
+			JOptionPane.INFORMATION_MESSAGE );	//输出output
 		
-		System.exit(0);
-	}
+		System.exit( 0 ); // terminate application
+		
+	} // end method main
 
-}
+} // end class SubString
