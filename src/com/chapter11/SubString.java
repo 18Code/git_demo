13e987bd36a804463b57f4ed68977b533be31231
wizcc <294116824@qq.com>
2018-08-10 09:44:30 +0800

    11.6

---
+++ src/com/chapter11/SubString.java
@@ -0,0 +1,11 @@
+package com.chapter11;
+//11.6
+public class SubString {
+	public static void main(String[] args) {
+		
+		//测试subString方法
+		String output = "Substring from index 20 to end is " + "\"" + letters.substring( 20 ) + "\"";
+		output += "Substring from index 3 up to 6 is " + "\"" + letters.substring( 3, 6 ) + "\"";
+	}
+
+}
