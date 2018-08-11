80471a279f7225a4dcff929147f918448b7e8ba9
gongfeifei <15230810870@163.com>
2018-08-11 21:19:56 +0800

    StringBufferCapLen补充

--- src/com/chapter11/StringBufferCapLen.java
+++ src/com/chapter11/StringBufferCapLen.java
@@ -7,8 +7,9 @@ public class StringBufferCapLen {
 	public static void main(String[] args) {
 
 		// feifei
-		StringBuffer buffer = new StringBuffer( "" );
-		String output = "";
+		StringBuffer buffer = new StringBuffer( "Hello, how are you?" );	//使用以一个String作为参数的StringBuffer构造函数，将这个StringBuffer对象初始化为Hello, how are you?
+		String output = "buffer = " + buffer.toString() + "\nlength = " +	
+				buffer.length() + "\ncapacity = " + buffer.capacity();	//将buffer的内容、长度和容量赋给output
 		
 		// 设置buffer对象的容量，若参数大于原容量的二倍加上2，则更改容量，否则不变
 		buffer.ensureCapacity( 75 );
