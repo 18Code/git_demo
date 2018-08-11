6f53134a12cebd565c2910c519fa6913c7f7ba05
teamOfSy <407626072@qq.com>
2018-08-11 11:14:11 +0800

    commit

--- src/com/chapter11/StringBufferInsert.java
+++ src/com/chapter11/StringBufferInsert.java
@@ -2,26 +2,37 @@ package com.chapter11;
 //11.14
 public class StringBufferInsert {
 	public static void main(String[] args) {
-		//wu
+		// 创建不同类型的对象
+		Object objectRef = "hello";
+		String string = "goodbye";
+		char charArray[] = { 'a', 'c', 'c', 'd', 'e', 'f' };
+		boolean booleanValue = true;
+		char characterValue = 'K';
+		int integerValue = 7;
+		long longValue = 10000000;
+		float floatValue = 2.5f;
+		double doubleValue = 33.33;
+		StringBuffer buffer = new StringBuffer();
+		
 		//书上20行---hu
 		buffer.insert( 0, objectRef );
-		buffer.insert( "  " );
+		buffer.insert( 0, " " );
 		buffer.insert( 0, string );
-		buffer.insert( "  " );
+		buffer.insert( 0, " " );
 		buffer.insert( 0, charArray );
-		buffer.insert( "  " );
+		buffer.insert( 0, " " );
 		buffer.insert( 0, charArray, 3, 3 );
-		buffer.insert( "  " );
+		buffer.insert( 0, " " );
 		buffer.insert( 0, booleanValue );
-		buffer.insert( "  " );
+		buffer.insert( 0, " " );
 		buffer.insert( 0, characterValue );
-		buffer.insert( "  " );
+		buffer.insert( 0, " " );
 		buffer.insert( 0, integerValue );
-		buffer.insert( "  " );
+		buffer.insert( 0, " " );
 		buffer.insert( 0 ,longValue);   
-		buffer.insert( "  " );
+		buffer.insert( 0, " " );
 		buffer.insert( 0 ,floatValue);
-		buffer.insert( "  " );
+		buffer.insert( 0, " " );
 		buffer.insert( 0,doubleValue);
 		
 		String output = "buffer after inserts:\n"+buffer.toString();  //将添加内容后的buffer输出
