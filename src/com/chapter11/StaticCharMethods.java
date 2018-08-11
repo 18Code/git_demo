e5c2e30e041f07d26c740f1e991df2bf0e90e274
wizcc <294116824@qq.com>
2018-08-11 14:30:15 +0800

    11.15

--- src/com/chapter11/StaticCharMethods.java
+++ src/com/chapter11/StaticCharMethods.java
@@ -8,7 +8,7 @@ import javax.swing.JFrame;
 // 11.15
 public class StaticCharMethods extends JFrame {
 	// feifei from 8 to 23
-	
+	private char c;
 	public StaticCharMethods(){
 		
 		
@@ -35,6 +35,20 @@ public class StaticCharMethods extends JFrame {
 	} // end constructor 
 		
 	// hu
+	//在outputArea中显示字符信息
+	private void buildOutput(){
+		
+		outputArea.setText( "is defined: " + Character.isDefined(c) + 
+				"\nis digit: " + Character.isDigit(c) + 
+				"\nis first character in a Java identifier: " + Character.isJavaIdentifierStart(c) +
+				"\nis part of a Java identifier: " + Character.isJavaIdentifierPart(c) +
+				"\nis letter: " + Character.isLetter(c) + 
+				"\nis letter or digit: " + Character.isLetterOrDigit(c) +
+				"\nis lower case: " + Character.isLowerCase(c) +
+				"\nis upper case: " + Character.isUpperCase(c) +
+				"\nto upper case: " + Character.toUpperCase(c) +
+				"\nto lower case: " + Character.toLowerCase(c) );
+	}
 	
 	
 	// shasha
