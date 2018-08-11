8b590d740d3e310ec66e76a6498f1e74892918e2
wizcc <294116824@qq.com>
2018-08-11 15:06:27 +0800

    11.17

---
+++ src/com/chapter11/OtherCharMethods.java
@@ -0,0 +1,16 @@
+package com.chapter11;
+//11.17
+//Character类的非静态方法
+
+public class OtherCharMethods {
+	public static void main(String[] args) {
+		
+		Character c1 = new Character( 'A' );
+		Character c2 = new Character( 'a' );
+		
+		String output = "c1 = " + c1.charValue() + "\nc2 = " + c2.toString();
+	}
+
+	
+
+}
