f92fec67436451dbd7f7b5196566c05a9a364f16
gongfeifei <15230810870@163.com>
2018-08-11 22:44:38 +0800

    OhterCharMethods part2

--- src/com/chapter11/OtherCharMethods.java
+++ src/com/chapter11/OtherCharMethods.java
@@ -9,6 +9,13 @@ public class OtherCharMethods {
 		Character c2 = new Character( 'a' );
 		
 		String output = "c1 = " + c1.charValue() + "\nc2 = " + c2.toString();
+		
+		//15-19
+		if (c1.equals(c2)) {	//if-else语句，使用equals方法来确实对象c1是否拥有与对象c2相同的内容，即两个对象中的字符是否相同
+			output += "\n\nc1 and c2 are equal";	//若相同，将字符串"c1 and c2 are equal"添加到output
+		} else {
+			output += "\n\nc1 and c2 are not equal";	//否则，将字符串"c1 and c2 are not equal"添加到output
+		}
 	}
 
 	
