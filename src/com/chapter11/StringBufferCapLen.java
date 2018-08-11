022c6267ca92a69f02df2d0e05acd045f1addd8d
teamOfSy <407626072@qq.com>
2018-08-11 09:48:01 +0800

    part 2

--- src/com/chapter11/StringBufferCapLen.java
+++ src/com/chapter11/StringBufferCapLen.java
@@ -3,10 +3,20 @@ package com.chapter11;
 
 public class StringBufferCapLen {
 	public static void main(String[] args) {
+		// feifei
+		StringBuffer buffer = new StringBuffer( "" );
+		String output = "";
+		
+		// 保证buffer指向的对象有至少75个字符的容量
+		buffer.ensureCapacity( 75 );
+		output += "\n\nNew capacity = " + buffer.capacity();
+		
 		//书17行
 		buffer.setLength( 10 );
 		output += "\n\nNew length = " + buffer.length() +
 				"\nbuf = " + buffer.toString();
+		
+		//shasha
 	}
 
 }
