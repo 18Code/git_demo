ba5d49568cfb8545c54aec01adb8543632975462
teamOfSy <407626072@qq.com>
2018-08-12 09:59:51 +0800

    还差一部分

--- src/com/chapter11/StaticCharacterMethods2.java
+++ src/com/chapter11/StaticCharacterMethods2.java
@@ -13,15 +13,14 @@ import javax.swing.JTextField;
 
 //11.16
 public class StaticCharacterMethods2 extends JFrame {
-
-	private JLabel prompt2;
-	private JTextField radixField;
-	private JTextField input;
-	private JButton toChar;
-	private JButton toInt;
-	private int digit;
-	private int radix;
-
+	// shasha from 8 to 25
+	private char c;
+	private int digit, radix;
+	private JLabel prompt1, prompt2;
+	private JTextField radixField, input;
+	private JButton toChar, toInt;
+	
+	
 	public StaticCharacterMethods2() {
 		// TODO Auto-generated constructor stub
 		Container container = null;
@@ -44,7 +43,26 @@ public class StaticCharacterMethods2 extends JFrame {
 						Character.forDigit(digit, radix));	//使用forDigit方法将整数digit转换为由整数radix所指定的数制中的相应字符
 			}
 		});	//addActionListener结束
+		
+		// 初始化JButton对象toInt
+		toInt = new	JButton( "Convert character to digit" );
+		toInt.addActionListener(	// 为toInt按钮设置监听程序
+			new ActionListener() { // anonymous  inner class
 				
+				@Override
+				public void actionPerformed( ActionEvent event ) {
+					digit = Integer.parseInt( input.getText() ); // 获取要转换的数字的string形式，转换成int型
+					radix = Integer.parseInt( radixField.getText() ); // 获取目标进制的string形式，转换成int型
+					JOptionPane.showMessageDialog( null,	// 弹出消息框
+						"Convert digit to character: " + 
+					Character.forDigit( digit,  radix ) ); // 第一个参数要转换的十进制数，第二个参数为目标进制参数
+					
+				} // end method actionPerformed
+				
+			} // end anonymous inner class
+			
+		); // end call to addActionListenner
+		
 		//hu
 		//71行
 		container.add( toChar );
