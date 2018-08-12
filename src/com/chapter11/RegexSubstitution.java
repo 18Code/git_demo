38fdd974a928d2b4b8562237c975b1a2633d4783
wizcc <294116824@qq.com>
2018-08-12 16:02:16 +0800

    11.23

--- src/com/chapter11/RegexSubstitution.java
+++ src/com/chapter11/RegexSubstitution.java
@@ -12,6 +12,13 @@ public class RegexSubstitution {
 		String output = "";
 		//19-28wu
 		//29-38hu
+		//将前3个数用‘digit’代替
+		for ( int i = 0; i < 3; i++ )
+			secondString = secondString.replaceFirst("\\d", "digit");
+		
+		output += "\nFirst 3 digits replaced by \"digit\" : " + secondString;
+		output += "\nString split at commas: [";
+		
 		//39-47shasha
 		String[] results = secondString.split(",\\s*");   //调用split方法分割一个由逗号分开的整数字符串，将分割结果保存在数组中
 		for(int i = 0;i<results.length;i++){  //遍历数组，输出分割的结果
