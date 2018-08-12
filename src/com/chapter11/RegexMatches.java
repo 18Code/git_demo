56d96e6faf8bd058db583a89fc86064a6b4b8cc6
Administrator <Administrator@PC-20170203NPVJ>
2018-08-12 12:14:23 +0800

    part1

---
+++ src/com/chapter11/RegexMatches.java
@@ -0,0 +1,15 @@
+package com.chapter11;
+
+import java.util.regex.Pattern;
+
+//11.24
+class RegexMatches {
+	public static void main(String[] args) {
+		//shasha 
+		String output = "";
+		Pattern expression = Pattern.compile("J.*\\d[0-35-9]-\\d\\d-\\d\\d"); //调用Pattern类的静态方法compile来创建一个Pattern
+		String string1 = "Jane's Birthday is 05-12-75\n"; //未完
+		
+	}
+
+}
