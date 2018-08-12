157c75a0641dc69c1414f8eb543c6bc1cc690216
Administrator <Administrator@PC-20170203NPVJ>
2018-08-12 11:37:03 +0800

    commit

--- src/com/chapter11/ValidateFrame.java
+++ src/com/chapter11/ValidateFrame.java
@@ -1,9 +1,15 @@
 package com.chapter11;
 
+import java.awt.Container;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 //11.21
 //用正则表达式验证用户输入
@@ -50,8 +56,52 @@ public class ValidateFrame extends JFrame {
 		firstTextField = new JTextField( 20 );
 		lastTextField = new JTextField( 20 );
 		
+		//shash 44-87
+		JPanel firstName = new JPanel();   //声明面板容器JPanel的对象，并将相应组件附加到对应的面板容器对象中
+		firstName.add(firstLabel);
+		firstName.add(firstTextField);
 		
+		JPanel lastName = new JPanel();
+		lastName.add(lastLabel);
+		lastName.add(lastTextField);
+		
+		JPanel address1 = new JPanel();
+		address1.add(addressLabel);
+		address1.add(addressTextField);
+		
+		JPanel address2 = new JPanel();
+		address2.add(cityLabel);
+		address2.add(cityTextField);
+		address2.add(stateLabel);
+		address2.add(stateTextField);
+		address2.add(zipLabel);
+		address2.add(zipTextField);
+		
+		JPanel phone = new JPanel();
+		phone.add(phoneLabel);
+		phone.add(phoneTextField);
+		
+		JPanel ok = new JPanel();
+		ok.add(okButton);
+		
+		Container container  = getContentPane(); //声明Container容器的对象
+		container.setLayout(new GridLayout(6,1));//设置页面布局为GridLayout网格布局，6*1
+		
+		container.add(firstName);   //将组建附加到container中
+		container.add(lastName);
+		container.add(address1);
+		container.add(address2);
+		container.add(phone);
+		container.add(ok);
+		
+		setSize(325,225);  //设置容器大小、可见
+		setVisible(true);
 		 
+	}
+	
+	
+	private void validateDate(){
+		
 	}
 
 }
