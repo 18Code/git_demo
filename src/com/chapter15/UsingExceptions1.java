516f81537a1bc784715e472f05759cdca319658d
gongfeifei <15230810870@163.com>
2018-08-14 14:50:37 +0800

    UsingExceptions1补充

--- src/com/chapter15/UsingExceptions1.java
+++ src/com/chapter15/UsingExceptions1.java
@@ -5,9 +5,6 @@ package com.chapter15;
 
 public class UsingExceptions1 {
 
-	public UsingExceptions1() {
-		// TODO Auto-generated constructor stub
-	}
 	public static void main(String[] args) {
 		try{
 			throwException();  //调用throwException方法
@@ -16,10 +13,11 @@ public class UsingExceptions1 {
 			System.err.println("Exception handled in main");   //输出在主方法中处理异常
 		}
 	}//end method main
-	public static void throwException(){
-		
-		
-		
+	public static void throwException() throws Exception{	//throwException方法
+		try {	//try块
+			System.out.println("Method throwException");	//输出信息
+			throw new Exception();	//抛出新的异常
+		}
 		//hu
 		//27-38
 		//catch的类型不对，所以异常没有被捕获
