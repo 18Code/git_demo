5c4b816411bb34fbdddb9e56150478ccc26b99ee
gongfeifei <15230810870@163.com>
2018-08-14 15:37:35 +0800

    注释

--- src/com/chapter16/ThreadTester.java
+++ src/com/chapter16/ThreadTester.java
@@ -3,23 +3,23 @@ package com.chapter16;
 //不同的时间间隔的多线程输出
 public class ThreadTester {	//public类ThreadTester
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	//程序的入口	主线程
 		
-		PrintThread thread1 = new PrintThread("thread1");
+		PrintThread thread1 = new PrintThread("thread1");	//创建并命名3个PrintThread对象
 		PrintThread thread2 = new PrintThread("thread2");
 		PrintThread thread3 = new PrintThread("thread3");
 		
-		System.err.println("Starting threads");
+		System.err.println("Starting threads");	//打印"Starting threads"信息
 		
-		thread1.start();
+		thread1.start();	//调用各个对象的start方法，使所有3个PrintThread对象从出生状态转换为就绪状态
 		thread2.start();
 		thread3.start();
 		
-		System.err.println("Threads started, main ends\n");
+		System.err.println("Threads started, main ends\n");	//打印"Threads started,main ends"信息
 		
-	}
+	}	//main方法结束
 	
-}
+}	//ThreadTester类结束
 
 	class PrintThread extends Thread{	//声明PrintThread类继承自Thread类
 		private int sleepTime;	//声明私有变量sleepTime
