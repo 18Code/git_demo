7564f54f9a9fef983f750129ea0a10cf954f609d
gongfeifei <15230810870@163.com>
2018-08-16 10:28:49 +0800

    SynchronizedBuffer part3

--- src/com/chapter16/SynchronizedBuffer.java
+++ src/com/chapter16/SynchronizedBuffer.java
@@ -6,7 +6,7 @@ public class SynchronizedBuffer implements Buffer{
 	private int occupiedBufferCount = 0; // 占用buffer线程计数
 	
 	// 加了关键字synchronized的set方法
-	public synchronized void set(){
+	public synchronized void set(int value){
 		//获取当前线程的名称
 		String name = Thread.currentThread().getName();
 		
@@ -17,9 +17,10 @@ public class SynchronizedBuffer implements Buffer{
 		//31-56 ---shasha
 		buffer = value;   //将接受的value值传给buffer
 		++occupiedBufferCount;  //将计数变量加一
-		displayState(name + " writes "+buffer);   显示当前状态
+		displayState(name + " writes "+buffer);   //显示当前状态
 		notify();   //调用notify方法，使除了当前线程外的其他线程从等待状态进入就绪状态
 	}//end method set
+
 	public synchronized int get(){
 		String name = Thread.currentThread().getName();  //获取当前线程的名称
 		while(occupiedBufferCount == 0){   //当缓冲区的数据量为0时
@@ -28,15 +29,26 @@ public class SynchronizedBuffer implements Buffer{
 				displayState(" Buffer empty."+ name +" waits.");
 				wait();  //调用wait方法，线程进入等待状态
 			}catch(InterruptedException exception){
-				
+				exception.printStackTrace();
 			}
 		}
+		
+		--occupiedBufferCount;
+		
+		displayState(name + " reads " + buffer);
+		
+		notify();
+		
+		return buffer;
 	}
 	
+	private void displayState(String operation) {
+		// TODO Auto-generated method stub
+		StringBuffer outputLine = new StringBuffer(operation);
+		outputLine.setLength(40);
+		outputLine.append(buffer + "\t\t" + occupiedBufferCount);
+		System.err.println(outputLine);
+		System.err.println();
+	}
 	
-	
-	
-	
-	
-
 }
