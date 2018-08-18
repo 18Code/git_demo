5e09ea4a9a835dd64778c0abd3fa9b19fb8b6e7c
gongfeifei <15230810870@163.com>
2018-08-18 22:06:52 +0800

    Consumer3 除run方法以外

--- src/com/chapter16/Consumer3.java
+++ src/com/chapter16/Consumer3.java
@@ -1,8 +1,19 @@
 package com.chapter16;
+
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+
 //16.13
 //生产者/消费者关系中的消费者
 public class Consumer3 extends Thread{
+	private Buffer sharedLocation;   //共享对象的引用
+	private JTextArea outputArea;   //声明JTextArea的引用，用于在run方法结束时输出一个消息
 	
+	public Consumer3(Buffer shared,JTextArea output){   //构造方法
+		super("Consumer3");//调用父类中构造方法
+		sharedLocation = shared;
+		outputArea = output;
+	}
 	
 	//19-32---shasha
 	public void run(){   //重写run方法
@@ -15,6 +26,10 @@ public class Consumer3 extends Thread{
 				exception.printStackTrace();
 			}
 		}
+		
+		String name = getName();	//获得当前线程名
+		SwingUtilities.invokeLater(new RunnableOutput(outputArea, "\nTotal " +
+				name + " consumed: " + sum + ".\n" + name + " terminated.\n"));	//RunnableOutput传给SwingUtilities类的invokeLater，以更新事件调度线程中GUI。在GUI中输出一个消息，指出该线程完成的值的消费总数，并即将终止。
 	}
 
 }
